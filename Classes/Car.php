4885c8c400bed48e26160998002915b69b0c855f
redvelv18 <diegodg8770@gmail.com>
2025-04-16 21:57:47 +0300

    Methods

--- Classes/Car.php
+++ Classes/Car.php
@@ -10,8 +10,18 @@ class Car {
     public function __construct($brand, $color = "none") {
         $this->brand = $brand;
         $this->color = $color;
+
     }
+
+    //Method
+
+    public function getCarInfo(){
+        return "Brand: " . $this->brand . ", Color: " . $this->color;
+    }
+
 }
 
 $car01 = new Car("Volvo", "Red");
+echo $car01->getCarInfo();
+echo "<br>";
 echo $car01->vehicleType;
