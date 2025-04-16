1c6b84a1e9024e9be3952e014992fa7dc5a0c25e
redvelv18 <diegodg8770@gmail.com>
2025-04-16 22:38:36 +0300

    Use oop php instead of procedural php

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
 </head>
 <body>
 
+<form action="includes/signup.inc.php" method="post">
+    <input type="text" name="username">
+    <input type="password" name="pwd">
+    <button>Signup</button>
+</form>
+
 <?php
     require_once 'Classes/Car.php';
 
